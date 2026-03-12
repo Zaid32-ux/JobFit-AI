993922f1d96931cc624bf31d2d5249146e49ead0
Zaid32-ux <Zgour5071@gmail.com>
2026-03-12 18:18:36 +0530

    connect auth routes in app.js

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import  dbConnection  from "./database/dbConnection.js";
 import { config } from "dotenv"
 import documentsRoutes from "./routes/documents.js";
+import authRoutes from "./routes/auth.js";
 
 config({ path: "./.env" });//calling again in main file bcz load it globally its recommended
 const app = express();
 app.use(express.json());
 
 app.use("/api/docs", documentsRoutes);
+app.use("/api/auth", authRoutes);
 
 // Socket.io
 const io = new IOServer(server, { cors: { origin: "*" } });

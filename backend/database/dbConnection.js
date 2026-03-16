d448d7be974d38f9e80b86a1692a19edd098d0fa
Zaid32-ux <Zgour5071@gmail.com>
2026-03-16 15:24:27 +0530

    add resumePendingJobs on server startup

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose"; //just mongoose import!
 import dotenv from "dotenv"
+import { resumePendingJobs } from "../jobs/worker.js";
 dotenv.config()
 const dbLink = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.3eexpys.mongodb.net/JobFit?retryWrites=true&w=majority&appName=Cluster0`;
 // console.log(dbLink);
  const dbConnection  = ()=>{
 mongoose.connect(dbLink)
-    .then(function (connection) {
+    .then(async function (connection) {
         console.log("connected to db")
+         // resume pending analysis jobs
+       await resumePendingJobs();
     }).catch(err => console.log(err))
  }
 export default dbConnection;

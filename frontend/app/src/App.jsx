18031f35ab5043a0107d629add7f71d08433dd67
Zaid32-ux <Zgour5071@gmail.com>
2026-03-17 15:53:56 +0530

    feat: add routing with protected routes and auth provider

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./pages/Login";
+import Navbar from "./components/elements/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SignupForm } from "./pages/Singup";
+import { NotFoundPage } from "./components/elements/NotFound";
+import FileUploader from "./pages/Upload";
+import "./App.css";
+import ScrollToTop from "./hooks/useScrollToTop";
+import HomePage from "./pages/Home";
+import ProfilePage from "./pages/Profile";
+import { Toaster } from "sonner";
+import Dashboard from "./pages/Dashboard";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const AppContent = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Toaster
+        position="top-center"
+        duration={4000}
+        richColors
+        theme="light"
+        closeButton
+      />
+      <Navbar />
+      <main className="flex min-h-[90vh] items-center justify-center ">
+        {/* <Login /> */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+                <HomePage />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/signup"
+            element={<SignupForm className="max-w-xl mx-auto w-full" />}
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/upload"
+            element={
+              <ProtectedRoute>
+                <FileUploader />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+function App() {
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <ScrollToTop />
+        <AppContent />
+      </AuthProvider>
+    </BrowserRouter>
+  );
+}
+export default App;
